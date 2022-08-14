5d7a3700eb1e3f11e1817c7590e677fb108db690
Ivan Tebenkov <fonbeauty@mail.ru>
2022-08-14 23:51:43 +0300

    succsess local tests run in selenoid

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,24 @@ import os
 
 from selene.support.shared import browser
 import pytest
+from selenium import webdriver
 
 
 @pytest.fixture(scope='function')
 def browser_management():
+    capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    browser.config.driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        desired_capabilities=capabilities)
+
     browser.config.base_url = os.getenv('selene.base_url', 'https://demoqa.com')
     browser.config.browser_name = os.getenv('selene.browser_name', 'chrome')
     browser.config.hold_browser_open = (
