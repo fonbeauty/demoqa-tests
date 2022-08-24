607e7aaf2f658b2342dd8d0d765196fa65e7d284
Ivan Tebenkov <fonbeauty@mail.ru>
2022-08-25 00:25:36 +0300

    step 2

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,19 @@
-from selenium import webdriver
+import os
 import pytest
+
+from selenium import webdriver
+from dotenv import load_dotenv
+
+
+def pytest_configure():
+    load_dotenv()
 
 
 @pytest.fixture()
 def browser():
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(os.getenv('CHROME_DRIVER_PATH'))
+    driver.get('https://github.com/')
+
+    yield driver
+
+    driver.quit()
--- tests/simple/test_pass.py
+++ tests/simple/test_pass.py
@@ -1,3 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
 
 
+def test_browser(browser: WebDriver):
+        # browser.find_element()
+    pass
