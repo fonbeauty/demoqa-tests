0d3c7bf875ad1aceb1f0f947b861abc3ec59554d
Ivan Tebenkov <fonbeauty@mail.ru>
2022-08-24 23:57:46 +0300

    init

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+import pytest
+
+
+@pytest.fixture()
+def browser():
+    driver = webdriver.Chrome()
--- tests/simple/test_pass.py
+++ tests/simple/test_pass.py
@@ -1,29 +1,3 @@
-import allure
+from selenium import webdriver
 
 
-@allure.feature('Простые тесты')
-@allure.story('Успешные тесты')
-def test_pass1():
-    with allure.step('Успешный тест 1'):
-        pass
-
-
-@allure.feature('Простые тесты')
-@allure.story('Успешные тесты')
-def test_pass2():
-    with allure.step('Успешный тест 2'):
-        pass
-
-
-@allure.feature('Простые тесты')
-@allure.story('Успешные тесты')
-def test_pass3():
-    with allure.step('Успешный тест 3'):
-        pass
-
-
-@allure.feature('Простые тесты')
-@allure.story('Успешные тесты')
-def test_pass4():
-    with allure.step('Успешный тест 4'):
-        pass
