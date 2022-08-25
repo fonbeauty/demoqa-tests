19f844fb4e73fc134e467c6e1bd0216b2ee0938f
Ivan Tebenkov <fonbeauty@mail.ru>
2022-08-26 00:44:07 +0300

    complete task

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,10 +9,10 @@ def pytest_configure():
     load_dotenv()
 
 
-@pytest.fixture()
-def browser():
+@pytest.fixture(params=[(1200, 900), (1024, 800), (400, 700), (500, 900)])
+def browser(request):
     driver = webdriver.Chrome(os.getenv('CHROME_DRIVER_PATH'))
-    driver.get('https://github.com/')
+    driver.set_window_size(request.param[0], request.param[1])
 
     yield driver
 
