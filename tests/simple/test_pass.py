9b3ec4e91e6740b31e739f558d0b6ca89547420f
18980620 <fonbeauty@mail.ru>
2022-10-03 22:43:32 +0300

    add tests

--- tests/simple/test_pass.py
+++ tests/simple/test_pass.py
@@ -1,43 +1,50 @@
-import time
-
 import pytest
-from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.common.by import By
+import requests
+from voluptuous import Schema
+from pytest_voluptuous import S
+
+schema = Schema(
+    {
+        'data': {
+            'id': int,
+            'email': str,
+            'first_name': str,
+            'last_name': str,
+            'avatar': str
+        },
+        'support': {
+            'url': str,
+            'text': str
+        }
+    }
+)
+
+def test_list_users():
+    response = requests.get('https://reqres.in/api/users?page=2', verify=False)
+    assert response.json()['page'] == 2
+    assert isinstance(response.json()['page'], int)
 
 
-def assert_h1(browser: WebDriver):
-    browser.find_element(By.CSS_SELECTOR, 'h1').text == 'Sign in to GitHub'
+def test_single_user():
+    response = requests.get('https://reqres.in/api/users/2', verify=False)
+    body = response.json()
+    body_data = response.json()['data']
+    try:
+        assert response.json()['data']['first_name'] == 'Janet'
+    except AssertionError:
+        assert_error = True
+
+    try:
+        assert isinstance(response.json()['data']['first_name'], int)
+    except AssertionError:
+        assert_error = True
+
+    if assert_error:
+        raise AssertionError
 
 
-def test_desktop_browser(browser: WebDriver):
-    width = browser.get_window_size().get("width")
-    height = browser.get_window_size().get("height")
-    if width//height >= 1:
-        browser.get('https://github.com/')
-        browser.find_element(By.CSS_SELECTOR, 'a[href="/login"').click()
-        # time.sleep(2)
-    else:
-        pytest.skip('Это разрешение экрана не для десктопной версии')
-    assert_h1(browser)
+def test_single_user_schema_validation():
+    response = requests.get('https://reqres.in/api/users/2', verify=False)
+    assert S(schema) == response.json()
 
 
-def test_mobile_browser(browser: WebDriver):
-    width = browser.get_window_size().get("width")
-    height = browser.get_window_size().get("height")
-    if width//height < 1:
-        browser.get('https://github.com/')
-        browser.find_element(By.CSS_SELECTOR, 'button[aria-label="Toggle navigation"').click()
-        browser.find_element(By.CSS_SELECTOR, 'a[href="/login"').click()
-        # time.sleep(2)
-    else:
-        pytest.skip('Это разрешение экрана не для мобильной версии')
-    assert_h1(browser)
-
-
-@pytest.mark.parametrize("browser", [(1000, 1000)], indirect=True)
-def test_indirect_size(browser: WebDriver):
-    browser.get('https://github.com/')
-    browser.find_element(By.CSS_SELECTOR, 'button[aria-label="Toggle navigation"').click()
-    browser.find_element(By.CSS_SELECTOR, 'a[href="/login"').click()
-    # time.sleep(2)
-    assert_h1(browser)
